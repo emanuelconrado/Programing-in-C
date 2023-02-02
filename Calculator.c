d11ac375ced0781b2877a64667cac82a6cc9f62c
Emanuel Conrado <emanuelthadeu45@gmail.com>
2023-02-02 14:48:44 -0300

    patch 1.3: Button to calculate another BMI

--- Calculator.c
+++ Calculator.c
@@ -2,7 +2,11 @@
 #include <math.h>
 
 /*Body Mass Index Calculator*/
-/*M and C are two variables to creat a finish button*/
+/*F is variable to creat a finish button*/
+/*Loop is a variable to control how many BMI do you want calculate*/
+/*Trash is a variable to eliminate "Enter" in the memorie buff*/
+
+
 
 /*Function to calculate BMI*/
 void BodyMass(float weight, float height, float *BMI){
@@ -23,27 +27,35 @@ void BmiStatus(float BMI){
     }else{
         puts("Extremely Obese");
     }
-    
+
 }
 
 int main(){
     
     float height, weight, BMI;
-    char M, C;
+    char F, loop, trash;
 
-    printf("Your Weight (KG):");
-    scanf("%f", &weight);
-    printf("Your Height (Metres) (Exemple: 1.70):");
-    scanf("%f", &height);
+    while(loop = 'y' || loop == 'Y'){
+        printf("Your Weight (KG):");
+        scanf("%f%c", &weight, &trash);
+        printf("Your Height (Metres) (Exemple: 1.70):");
+        scanf("%f%c", &height, &trash);
 
-    BodyMass(weight, height, &BMI);
+        BodyMass(weight, height, &BMI);
 
-    BmiStatus(BMI);
+        BmiStatus(BMI);
 
-    printf("BMI: %.2f\n", BMI);
+        printf("BMI: %.2f\n", BMI);
 
+        puts("Type 'Y' to calculate another BMI or 'N' to exit");
+        scanf("%c%c",&loop, &trash);
+
+        if(loop == 'n' || loop == 'N'){
+            break;
+        }
+    }
     puts("Press Enter to finish the program");
-    scanf("%c%c", &M, &C);
+    scanf("%c", &F);
 
     return 0;
 }
