bfac6267dfd99b8919114fdafaa5e500eedbdfa4
Emanuel Conrado <emanuelthadeu45@gmail.com>
2023-02-02 11:46:56 -0300

    Patch 1.1: add Function BodyMass

--- Calculator.c
+++ Calculator.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 /*Body Mass Index Calculator*/
 /*M and C are two variables to creat a finish button*/
 
+/*Function to calculate BMI*/
+void BodyMass(float weight, float height, float *BMI){
+
+    *BMI = weight/(height*height);
+
+}
+
 int main(){
     
     float height, weight, BMI;
-    char m, c;
+    char M, C;
 
     printf("Your Weight (KG):");
     scanf("%f", &weight);
     printf("Your Height (Metres) (Exemple: 1.70):");
     scanf("%f", &height);
 
-    BMI = weight/(height*height); 
+    BodyMass(weight, height, &BMI);
 
     if(BMI < 18.5){
         puts("Underweight");
@@ -28,7 +36,7 @@ int main(){
     printf("BMI: %.2f\n", BMI);
 
     puts("Press Enter to finish the program");
-    scanf("%c%c", &m, &c);
+    scanf("%c%c", &M, &C);
 
     
 
